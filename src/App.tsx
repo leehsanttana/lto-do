2e63ff5381c12866ce143b7ecf141cd5570546fc
Leonam Santana <leonamsanttana@gmail.com>
2022-01-23 14:34:31 -0300

    escrevendo components em minusculo e criando o switch theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import React, {useState} from 'react';
 import GlobalStyle from './styles/global';
 import {ThemeProvider} from 'styled-components'
 import light from './styles/themes/light';
+import dark from './styles/themes/dark'
 
-import Header from './Components/Header';
-import Section from './Components/Section';
+import Header from  './components/Header';
+import Section from './components/Section';
 
-export default function App() {
+const App: React.FC  = () => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  };
 
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
+      <Header toggleTheme={toggleTheme} />
       <Section />
     </ThemeProvider>
   );
 };
+
+export default App;
